a1a26e1cc125cc5214c457aea3374f2fcb695262
Vlado Dimitriev <vdimitriev@bol.com>
2021-05-02 09:51:43 +0200

    Add login functionality.

--- src/mk/vedmak/bitmail/controller/LoginWindowController.java
+++ src/mk/vedmak/bitmail/controller/LoginWindowController.java
@@ -7,6 +7,9 @@ import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 import mk.vedmak.bitmail.EmailManager;
+import mk.vedmak.bitmail.controller.services.EmailLoginResult;
+import mk.vedmak.bitmail.controller.services.LoginService;
+import mk.vedmak.bitmail.model.EmailAccount;
 import mk.vedmak.bitmail.view.ViewFactory;
 
 public class LoginWindowController extends BaseController {
@@ -31,10 +34,36 @@ public class LoginWindowController extends BaseController {
 
     @FXML
     void loginButtonAction() {
+        if(fieldsAreValid()) {
+            EmailAccount emailAccount = new EmailAccount(emailAddressField.getText(), passwordField.getText());
+            LoginService loginService = new LoginService(emailAccount, emailManager);
+            EmailLoginResult emailLoginResult = loginService.login();
+            switch(emailLoginResult) {
+                case SUCCESS:
+                    System.out.println("login succesfull !!" + emailAccount);
+                    break;
+                default:
+                    System.out.println("login not succesfull !!");
+            }
+        }
         System.out.println("Login button clicked !!");
         viewFactory.showMainWindow();
         final Stage stage = (Stage) errorLabel.getScene().getWindow();
         viewFactory.closeStage(stage);
+    }
+
+    private boolean fieldsAreValid() {
+        if(emailAddressField.getText().isEmpty()) {
+            errorLabel.setText("Please fill email");
+            return false;
+        }
+
+        if(passwordField.getText().isEmpty()) {
+            errorLabel.setText("Please fill email");
+            return false;
+        }
+
+        return true;
     }
 
 }
--- src/mk/vedmak/bitmail/controller/services/LoginService.java
+++ src/mk/vedmak/bitmail/controller/services/LoginService.java
@@ -3,6 +3,9 @@ package mk.vedmak.bitmail.controller.services;
 import mk.vedmak.bitmail.EmailManager;
 import mk.vedmak.bitmail.model.EmailAccount;
 
+import javax.mail.*;
+
+
 public class LoginService {
 
     EmailAccount emailAccount;
@@ -13,7 +16,34 @@ public class LoginService {
         this.emailManager = emailManager;
     }
 
-    public void login() {
+    public EmailLoginResult login() {
         System.out.println("Login method called in login service.");
+        Authenticator authenticator = new Authenticator() {
+            @Override
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication(emailAccount.getAddress(), emailAccount.getPassword());
+            }
+        };
+
+        try {
+            Session session = Session.getInstance(emailAccount.getProperties(), authenticator);
+            Store store = session.getStore("imaps");
+            store.connect(emailAccount.getProperties().getProperty("incomingHost"), emailAccount.getAddress(), emailAccount.getPassword());
+            emailAccount.setStore(store);
+            return EmailLoginResult.SUCCESS;
+        } catch(AuthenticationFailedException e) {
+            e.printStackTrace();
+            return EmailLoginResult.FAILED_BY_CREDENTIALS;
+        } catch (NoSuchProviderException e) {
+            e.printStackTrace();
+            return EmailLoginResult.FAILED_BY_NETWORK;
+        } catch (MessagingException e) {
+            e.printStackTrace();
+            return EmailLoginResult.FAILED_BY_UNEXPECTED_ERROR;
+        } catch (Exception ex) {
+            System.out.println(ex.getMessage());
+            ex.printStackTrace();
+            return EmailLoginResult.FAILED_BY_UNEXPECTED_ERROR;
+        }
     }
 }
