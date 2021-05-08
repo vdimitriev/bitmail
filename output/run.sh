caabb2f221cff2f582deb71cacaf022370d160fd
Vlado Dimitriev <vdimitriev@bol.com>
2021-05-08 17:15:15 +0200

    Add bitmail jar file

---
+++ output/run.sh
@@ -0,0 +1,2 @@
+#java --module-path /Users/vdimitriev/Projects/private/javafx/javafx-sdk-11.0.2/ --add-modules javafx.controls,javafx.fxml,javafx.graphics,javafx.web -jar bitmail.jar
+java --module-path javafx/ --add-modules javafx.controls,javafx.fxml,javafx.graphics,javafx.web -jar bitmail.jar
